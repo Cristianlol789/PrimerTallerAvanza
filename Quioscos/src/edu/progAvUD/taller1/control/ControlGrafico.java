6bc39df54d8896c4d6dab9e946fc18c5ce782c7e
AndPre <afpreciadoc@udistrital.edu.co>
2025-04-08 10:04:56 -0500

    Cracion paquete vista (inyeccion Control), cambios en ControlGrafico y creacion de paqute imagenes

--- Quioscos/src/edu/progAvUD/taller1/control/ControlGrafico.java
+++ Quioscos/src/edu/progAvUD/taller1/control/ControlGrafico.java
@@ -1,5 +1,28 @@
 package edu.progAvUD.taller1.control;
 
-public class ControlGrafico {
+import edu.progAvUD.taller1.vista.VentanaPrincipal;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ControlGrafico implements ActionListener {
+    
+    private VentanaPrincipal ventanaPrincipal;
+
+    public ControlGrafico() {
+        this.ventanaPrincipal = new VentanaPrincipal(this);
+        ventanaPrincipal.mostrarPanel(ventanaPrincipal.panelInicial);
+        
+        ventanaPrincipal.panelInicial.jButtonSiguiente.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==ventanaPrincipal.panelInicial.jButtonSiguiente){
+            
+        }
+    }
+    
+    
+    
     
 }
