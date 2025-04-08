dd29ee57d973002c933385ea1685901cf4fa4f5b
Juan Diaz <juanchodiazr777@gmail.com>
2025-04-07 22:34:10 -0500

    Probando JuanBranch/sugerencia cliente

--- Quioscos/src/edu/progAvUD/taller1/modelo/Cliente.java
+++ Quioscos/src/edu/progAvUD/taller1/modelo/Cliente.java
@@ -1,5 +1,30 @@
 package edu.progAvUD.taller1.modelo;
 
 public class Cliente {
+    private String nombreCliente;
+    private double dineroDisponible;
+    private double puntosAcumulados;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nombreCliente, double dineroDisponible, double puntosAcumulados) {
+        this.nombreCliente = nombreCliente;
+        this.dineroDisponible = dineroDisponible;
+        this.puntosAcumulados = puntosAcumulados;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public double getDineroDisponible() {
+        return dineroDisponible;
+    }
+
+    public double getPuntosAcumulados() {
+        return puntosAcumulados;
+    }
+    
     
 }
