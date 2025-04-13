abe74c2ba6e32d9a108a4e1d8f20d256f11ad59b
Cristianlol789 <cristianaguilartreras08@gmail.com>
2025-04-13 03:41:40 -0500

    Cambio nullPointed

--- Quioscos/src/edu/progAvUD/taller1/control/ControlPedido.java
+++ Quioscos/src/edu/progAvUD/taller1/control/ControlPedido.java
@@ -1,39 +1,37 @@
 package edu.progAvUD.taller1.control;
 
+import edu.progAvUD.taller1.control.ControlPrincipal;
 import edu.progAvUD.taller1.modelo.Cliente;
 import edu.progAvUD.taller1.modelo.OpcionesMenu;
 import edu.progAvUD.taller1.modelo.Pedido;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-/**
- *Esta clase se encarga de hacer el pedido de el cliente, para ello crea un numero de pedido y lo guarda en un arreglo, en el pedido se puede ver como pago la persona
- * @author Cristianlol789
- */
 public class ControlPedido {
 
     private ControlPrincipal controlPrincipal;
     private int numeroTicket;
-    private ArrayList<Pedido> pedidos;
+    private ArrayList<Pedido> pedidos;  // Declaration remains here
     
     /**
-     *Inicializa las variables para guardar los parametros de los demas metodos
-     *@param controlPrincipal se usa como referencia para comunicarse con el control principal
+     * Inicializa las variables para guardar los parametros de los demas metodos
+     * @param controlPrincipal se usa como referencia para comunicarse con el control principal
      */
     public ControlPedido(ControlPrincipal controlPrincipal) {
         this.controlPrincipal = controlPrincipal;
         this.numeroTicket = 0;
+        this.pedidos = new ArrayList<>();  // Initialize the list
     }
     
     /**
-     *Da el numero de ticket de para reclamar el pedido
+     * Da el numero de ticket de para reclamar el pedido
      */
     public void generarNumTicket(){
         this.numeroTicket += 1; 
     }
     
     /**
-     *El metodo crea el pedido de la persona y pone el ticket en activo para poder reclamar su pedido
+     * El metodo crea el pedido de la persona y pone el ticket en activo para poder reclamar su pedido
      * @param cedula se usa para buscar la persona por medio de su control
      * @param metodoDePago se mira con que opcion la persona pago;
      */
@@ -44,7 +42,7 @@ public class ControlPedido {
         double precioTotal = controlPrincipal.getControlOpcionesMenu().getPrecioTotalPedido();
         Pedido pedido = new Pedido(this.numeroTicket, cliente, opcionesMenu, precioTotal, metodoDePago);
         pedido.setEstadoTicket(true);
-        pedidos.add(pedido);
+        pedidos.add(pedido);  
         controlPrincipal.getControlOpcionesMenu().vaciarHashMap();
         controlPrincipal.getControlOpcionesMenu().vaciarPrecio();
         controlPrincipal.getControlQuiosco().cambiarListaPedidos(pedidos);
